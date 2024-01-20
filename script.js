77556594df510c75c89d37f5a51397bd000fecfc
HidekiNishida <147544986+HidekiNishida@users.noreply.github.com>
2024-01-20 16:07:52 -0300

    Adicionei novos itens

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Lógica adicional, se necessário
+    // Por exemplo, esconder a animação após um certo tempo
+    setTimeout(function() {
+        document.getElementById("abertura").style.display = "none";
+    }, 3000); // Tempo em milissegundos
+});
