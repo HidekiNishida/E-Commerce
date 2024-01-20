86f41cba3d9e557f875388f47e1b9fc4c31ee16d
HidekiNishida <147544986+HidekiNishida@users.noreply.github.com>
2024-01-20 16:16:28 -0300

    Teste 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Lógica adicional, se necessário
-    // Por exemplo, esconder a animação após um certo tempo
+    // Aguarde um breve momento antes de iniciar a animação
     setTimeout(function() {
-        document.getElementById("abertura").style.display = "none";
-    }, 3000); // Tempo em milissegundos
+        var whiteBox = document.querySelector('.white-box');
+        
+        // Animação de aparecimento
+        whiteBox.style.opacity = '1';
+        whiteBox.style.transform = 'scale(1)';
+        
+        // Aguarde mais um momento antes de esconder a animação
+        setTimeout(function() {
+            document.getElementById('abertura').style.display = 'none';
+        }, 2000); // Ajuste conforme necessário
+    }, 1000); // Ajuste conforme necessário
 });
